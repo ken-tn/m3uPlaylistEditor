5ca4d094f04410dcf55754705c149f4583d462c0
kent <38088995+kent911t@users.noreply.github.com>
2023-04-30 18:25:43 +0100

    backup, sql + models, parsers wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,42 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'utilities/file_utils.dart';
+import 'utilities/utils_file.dart';
 
-void main() {
+void main() async {
+  // Avoid errors caused by flutter upgrade.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Open the database and store the reference.
+  final database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+    join(await getDatabasesPath(), 'm3u_playlist_data.db'),
+    // When the database is first created, create a table to store the models.
+    onCreate: (db, version) {
+      // Run the CREATE TABLE statement on the database.
+
+      // TODO: use json tag data instead of explicit tags for files?
+      return db.execute(
+        """
+        CREATE TABLE Playlist(path TEXT PRIMARY KEY);
+        CREATE TABLE Audio(id INTEGER PRIMARY KEY AUTOINCREMENT, path TEXT, type TEXT);
+        CREATE TABLE Mp3ID3v1(id INTEGER, title TEXT, artist TEXT,
+            album TEXT, year INTEGER, genre TEXT,
+            FOREIGN KEY(id) REFERENCES Audio(id))
+        """,
+      );
+    },
+    // Set the version. This executes the onCreate function and provides a
+    // path to perform database upgrades and downgrades.
+    version: 1,
+  );
+
   runApp(MyApp());
 }
 
@@ -47,7 +79,8 @@ class MyAppState extends ChangeNotifier {
   }
 
   Future<void> requestPermission(List<Permission> permission) async {
-    Map<Permission, PermissionStatus> statuses = await permission.request();
+    // Map<Permission, PermissionStatus> statuses = await permission.request();
+    await permission.request();
   }
 }
 
---
+++ lib/utilities/utils_mp3.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+import 'package:m3u_playlist/models/model_mp3_id3v1.dart';
+
+Object toMP3(FileSystemEntity file) {
+  var mp3 = const Mp3ID3v1();
+
+  return 1;
+}
