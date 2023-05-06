879f634b476d3ab48dbe5792b162d6011a612407
kent <38088995+kent911t@users.noreply.github.com>
2023-05-06 17:51:10 +0100

    loaded songs persist

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:m3u_playlist/utilities/app_state.dart';
-import 'package:m3u_playlist/utilities/file_utils.dart';
 import 'package:m3u_playlist/widgets/playlist_widget.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,7 +16,7 @@ class _HomePageState extends State<HomePage> {
     var appState = context.watch<AppState>();
     var musicData = appState.musicData;
 
-    Future<void> _pullRefresh() async {
+    Future<void> pullRefresh() async {
       appState.updateMusicData();
     }
 
@@ -34,7 +32,7 @@ class _HomePageState extends State<HomePage> {
             child: const Icon(Icons.add),
           ),
           body: RefreshIndicator(
-            onRefresh: _pullRefresh,
+            onRefresh: pullRefresh,
             child: PlaylistWidget(snapshot: snapshot),
           ),
         );
