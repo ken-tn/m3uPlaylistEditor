bd28ac17d0f06edcdf866b457754bafc2bfc85c9
kent <kent911t@gmail.com>
2023-05-12 00:17:58 +0100

    add build files

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+@REM Change DEVICE_ID to your connected device id (adb devices -l)
+SET DEVICE_ID=0
+
+
+
+IF %DEVICE_ID% == 0 goto :fail
+SET RELEASE_PATH=%~dp0build\app\outputs\bundle\release
+CALL flutter pub get
+CALL flutter build appbundle
+IF EXIST "%RELEASE_PATH%\app-release.apks" DEL /F "%RELEASE_PATH%\app-release.apks"
+java -jar bundletool-all-1.14.1.jar build-apks --connected-device --bundle=%RELEASE_PATH%/app-release.aab --output=%RELEASE_PATH%/app-release.apks --device-id=%DEVICE_ID%
+java -jar bundletool-all-1.14.1.jar install-apks --apks=%RELEASE_PATH%/app-release.apks --device-id=%DEVICE_ID%
+echo Build installed.
+EXIT /B 0
+
+:fail
+echo Set DEVICE_ID in %~dp0build.bat
+EXIT /B 1
