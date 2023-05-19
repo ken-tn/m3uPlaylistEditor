9e4e4bacbefe5b8c0205425dd4f21e9b32f14b55
kent <kent911t@gmail.com>
2023-05-19 17:28:16 +0100

    fix previous commit

--- apk_build.bat
+++ apk_build.bat
@@ -7,10 +7,10 @@ CALL flutter build appbundle
 IF EXIST "%RELEASE_PATH%\app-release.apks" DEL /F "%RELEASE_PATH%\app-release.apks"
 java -jar bundletool-all-1.14.1.jar build-apks --bundle=%RELEASE_PATH%/app-release.aab --output=%RELEASE_PATH%/app-release.apks --mode=universal
 echo Extracting.
-IF EXIST "%RELEASE_PATH%\universal.apks" DEL /F "%RELEASE_PATH%\universal.apks"
 IF EXIST "%RELEASE_PATH%\universal.apk" DEL /F "%RELEASE_PATH%\universal.apk"
 IF EXIST "%RELEASE_PATH%\playlist_editor.apk" DEL /F "%RELEASE_PATH%\playlist_editor.apk"
 7z e "%RELEASE_PATH%\app-release.apks" -o"%RELEASE_PATH%" universal.apk
 REN "%RELEASE_PATH%\universal.apk" playlist_editor.apk 
+IF EXIST "%RELEASE_PATH%\app-release.apks" DEL /F "%RELEASE_PATH%\app-release.apks"
 echo Build completed.
 EXIT /B 0
