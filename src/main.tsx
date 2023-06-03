06e353733660cb1a70dafc73e0b7aa68831a0d63
kent <kent911t@gmail.com>
2023-06-03 22:53:05 +0100

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "privacy",
+        element: <App />,
+    }
+], {basename: "/m3uPlaylistEditor"});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
